f34df6aca4910764b5db476cc758ab44bc3b9236
carlospunkk <cromulo.romulo@gmail.com>
2023-01-08 15:44:48 -0300

    arquivos aula 1 back-end

---
+++ type-number.js
@@ -0,0 +1,15 @@
+// tipo number
+/*const numero = 1;
+const numero2 = 2;
+const result = numero + numero2;*/
+
+// tipo string 
+const texto = 2
+const mult = 1;
+
+const operacao = mult * texto
+console.log(operacao)
+
+
+
+
