2533726eee524d12a1c66a0d77229ffa9bbdd68d
carlospunkk <cromulo.romulo@gmail.com>
2023-01-08 21:05:58 -0300

    aula 2 let,const,var

--- type-number.js
+++ type-number.js
@@ -3,12 +3,52 @@
 const numero2 = 2;
 const result = numero + numero2;*/
 
-// tipo string 
-const texto = 2
+// tipo string
+/*const texto = 2
 const mult = 1;
 
 const operacao = mult * texto
-console.log(operacao)
+console.log(operacao)*/
+
+//var
+/*var altura = 5;
+var comprimento = 7;
+var area;
+area = altura * comprimento;
+console.log(area)*/
+
+//let
+/*let altura = 4;
+let comprimento = 3;
+let forma = "retangulo"
+let area;
+
+if (forma === 'retangulo') {
+  area = altura * comprimento
+} else {
+  area = (altura * comprimento) / 2
+}
+
+console.log(area)
+
+*/
+
+
+
+//const 
+const forma = 'quadrado'
+const altura = 5;
+const comprimento = 7;
+let area;
+
+
+if (forma === 'trinagulo') {
+  area = altura * comprimento;
+} else {
+  area = (altura * comprimento) / 2;
+}
+
+console.log(area)
 
 
 
